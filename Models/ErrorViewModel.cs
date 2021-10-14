375bbe169f640bf2f18bd9ffe884e7734b23ba0e
phungngochuyhoang <phungngochuyhoang@gmail.com>
2021-10-14 21:52:14 +0700

    Start code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tempWebSI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
