dbe03a5a55a12562d0ab04cf253e7b9cf7f3dd3f
phungngochuyhoang <phungngochuyhoang@gmail.com>
2021-10-25 16:44:27 +0700

    page find

--- FE_template/Assets/js/index.js
+++ FE_template/Assets/js/index.js
@@ -1,31 +1,35 @@
-$(function() {
-    const $submennu = $('.submenu');
-    const $submenuItem = $('.submenu-item');
+const $submennu = $(".submenu");
+const $submenuItem = $(".submenu-item");
 
-    const constObject = {
-        speedWeb: 200
-    }
+const constObject = {
+  speedWeb: 200,
+};
+// only with menu level 1
+const dropdownTreeLevelOne = function (parent, child, speedTime) {
+  $submenuItem.hide();
+  parent.each(function (i, el) {
+    $(el).click(function () {
+      child.each((index, element) => {
+        if ($(element).data("submenu-id") == i + 1)
+          $(element).slideToggle(speedTime);
+      });
+    });
+  });
+};
+// read url file, but logic works with imgs
+const readURL = function (input, imagePreview) {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      $(imagePreview).css("background-image", `url(${e.target.result})`);
+      $(imagePreview).hide();
+      $(imagePreview).fadeIn(650);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+};
 
-
-    const dropdownTreeLevelOne = function (parent, child, speedTime) {
-        $submenuItem.hide();
-        parent.each(function (i, el) {
-            $(el).click(function () {
-                child.each((index, element) => {
-                    if($(element).data('submenu-id') == (i + 1)) $(element).slideToggle(speedTime);                  
-                })
-            })
-        })        
-    }
-
-   
-
-    $(document).ready(function () {
-        //home 
-        dropdownTreeLevelOne($submennu, $submenuItem, constObject.speedWeb);
-
-    })
-    
-
-})
-
+$(document).ready(function () {
+  //work home
+  dropdownTreeLevelOne($submennu, $submenuItem, constObject.speedWeb);
+});
